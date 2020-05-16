959ad994c151e3e45e3a21c3496978b026402043
matthew snoddy <djfdyuruiry@users.noreply.github.com>
2020-05-16 23:54:47 +0100

    v2.1.0
    - Adding support for large amounts of dependencies in project package
    - Code refactoring
    - Added test for large dependency scenario

--- test/test.sh
+++ test/test.sh
@@ -31,7 +31,13 @@ runTests() {
   excludedAdvisories="3,8,28,29,535,880,1469"
 
   cd "${scriptPath}"
+
+  rm -f package.json
+  rm -f yarn.lock
   rm -f .iyarc
+
+  cp vunerable-package.json package.json
+  cp vunerable-yarn.lock yarn.lock
 
   #test 1
   callIya
@@ -54,13 +60,15 @@ runTests() {
   echo "${excludedAdvisories}" >> .iyarc
 
   callIya
-  testExitCode "they are excluded in .iyarc and comments are present" "0" "$?"
+  testExitCode "they are excluded in .iyarc file with comments" "0" "$?"
 
   #test 5
   echo "${excludedAdvisories}" > .iyarc
 
   callIya -e 1469
   testExitCode "vulnerabilities are present and they are excluded in .iyarc but exclusions are in the command line" "6" "$?"
+
+  rm -f .iyarc
 
   #test 6
   callIya -i
@@ -91,11 +99,27 @@ runTests() {
   echo "28,29,1469" > .iyarc
   callIya -s high 
   testExitCode "they are excluded in .iyarc and min severity is high" "0" "$?"
+
+  rm -f .iyarc
+
+  #test 13  
+  rm -f package.json
+  rm -f yarn.lock
+
+  cp huge-package.json package.json
+  cp huge-yarn.lock yarn.lock
+
+  callIya -s high -e 1486 
+  testExitCode "the package JSON results in huge JSON audit output (~1.5GB)" "0" "$?"
 }
 
 runTests
 
 if [ "${testFailed}" == true ]; then
+  echo "Test Result: FAILURE"
   echo "There were test failures"
+
   exit 1
+else
+  echo "Test Result: PASS"
 fi
