38d5fd4155eaafbe6ccdaf935fbcf91646ae4414
matthew snoddy <djfdyuruiry@users.noreply.github.com>
2023-07-29 22:51:41 +0100

    Fixing test scrips (New advisories raised) (#34)

--- test/test.sh
+++ test/test.sh
@@ -29,136 +29,149 @@ callIya() {
 }
 
 runTests() {
-  cd "${scriptPath}"
+  cd "/tmp"
+  rm -rf /tmp/* &> /dev/null || :
 
   rm -f package.json
   rm -f yarn.lock
+  rm -rf .yarn
+  rm -rf node_modules
   rm -f .iyarc
 
-  cp vunerable-package.json package.json
-  cp vunerable-yarn.lock yarn.lock
+  cp "${scriptPath}/vunerable-package.json" package.json
+  cp "${scriptPath}/vunerable-yarn.lock" yarn.lock
+
+  yarn install
 
   # test 1
   callIya
-  testExitCode "not excluded" "9" "$?"
+  testExitCode "not excluded" "12" "$?"
 
   # test 2
-  excludedAdvisories=$(<mocks/test-2.args)
+  excludedAdvisories=$(<${scriptPath}/mocks/test-2.args)
   callIya -e "${excludedAdvisories}"
   testExitCode "vulnerabilities are present and they are excluded on the command line" "0" "$?"
 
   # test 3
   touch .iyarc
-  cp mocks/test-3.iyarc .iyarc
+  cp ${scriptPath}/mocks/test-3.iyarc .iyarc
   callIya
   testExitCode "vulnerabilities are present and they are excluded in .iyarc" "0" "$?"
   rm .iyarc
 
   # test 4
   touch .iyarc
-  cp mocks/test-4.iyarc .iyarc
+  cp ${scriptPath}/mocks/test-4.iyarc .iyarc
   callIya
   testExitCode "they are excluded in .iyarc file with comments" "0" "$?"
 
   # test 5
   rm -f .iyarc
   touch .iyarc
-  cp mocks/test-5.iyarc .iyarc
-  callIya -e 1469,1594,GHSA-3fw8-66wf-pr7m,GHSA-42xw-2xvc-qx8m
-  testExitCode "vulnerabilities are present and they are excluded in .iyarc but exclusions are in the command line" "7" "$?"
+  cp ${scriptPath}/mocks/test-5.iyarc .iyarc
+  callIya -e GHSA-3fw8-66wf-pr7m,GHSA-42xw-2xvc-qx8m,GHSA-f9cm-p3w6-xvr3,GHSA-gpvr-g6gh-9mc2
+  testExitCode "vulnerabilities are present and they are excluded in .iyarc but exclusions are in the command line" "8" "$?"
 
   # test 6
   rm -f .iyarc
   touch .iyarc
-  cp mocks/test-6.iyarc .iyarc
+  cp ${scriptPath}/mocks/test-6.iyarc .iyarc
   callIya -i
-  testExitCode "dev dependencies flag is present then dev vulnerabilities are ignored" "6" "$?"
+  testExitCode "dev dependencies flag is present then dev vulnerabilities are ignored" "9" "$?"
 
   # test 7
   callIya -s moderate
-  testExitCode "min severity is moderate" "8" "$?"
+  testExitCode "min severity is moderate" "11" "$?"
 
   # test 8
   callIya -s high
-  testExitCode "min severity is high" "6" "$?"
+  testExitCode "min severity is high" "7" "$?"
 
   # test 9
   # Moderate values = GHSA-gpvr-g6gh-9mc2,GHSA-wrvr-8mpx-r7pp
   # High Severity values = GHSA-jjv7-qpx3-h62q,GHSA-f9cm-p3w6-xvr3,GHSA-gqgv-6jq5-jjj9,GHSA-42xw-2xvc-qx8m,GHSA-4w2v-q235-vp99,GHSA-cph5-m8f7-6c5x
-  callIya -s moderate -e GHSA-gpvr-g6gh-9mc2,GHSA-wrvr-8mpx-r7pp,GHSA-jjv7-qpx3-h62q,GHSA-f9cm-p3w6-xvr3,GHSA-gqgv-6jq5-jjj9,GHSA-42xw-2xvc-qx8m,GHSA-4w2v-q235-vp99,GHSA-cph5-m8f7-6c5x
+  callIya -s moderate -e GHSA-gpvr-g6gh-9mc2,GHSA-wrvr-8mpx-r7pp,GHSA-jjv7-qpx3-h62q,GHSA-f9cm-p3w6-xvr3,GHSA-gqgv-6jq5-jjj9,GHSA-42xw-2xvc-qx8m,GHSA-cph5-m8f7-6c5x,GHSA-hrpp-h998-j3pp,GHSA-rch9-xh7r-mqgw,GHSA-4w2v-q235-vp99
   testExitCode "they are excluded on the command line and min severity is moderate" "0" "$?"
 
   # test 10
   # High Severity values = GHSA-jjv7-qpx3-h62q,GHSA-f9cm-p3w6-xvr3,GHSA-gqgv-6jq5-jjj9,GHSA-42xw-2xvc-qx8m,GHSA-4w2v-q235-vp99,GHSA-cph5-m8f7-6c5x
-  callIya -s high -e GHSA-jjv7-qpx3-h62q,GHSA-f9cm-p3w6-xvr3,GHSA-gqgv-6jq5-jjj9,GHSA-42xw-2xvc-qx8m,GHSA-4w2v-q235-vp99,GHSA-cph5-m8f7-6c5x
+  callIya -s high -e GHSA-jjv7-qpx3-h62q,GHSA-f9cm-p3w6-xvr3,GHSA-gqgv-6jq5-jjj9,GHSA-42xw-2xvc-qx8m,GHSA-cph5-m8f7-6c5x,GHSA-hrpp-h998-j3pp
   testExitCode "they are excluded on the command line and min severity is high" "0" "$?"
 
   # test 11
   rm -f .iyarc
   touch .iyarc
-  cp mocks/test-11.iyarc .iyarc
+  cp ${scriptPath}/mocks/test-11.iyarc .iyarc
   callIya -s moderate
   testExitCode "they are excluded in .iyarc and min severity is moderate" "0" "$?"
 
   # test 12
   rm -f .iyarc
   touch .iyarc
-  cp mocks/test-12.iyarc .iyarc
+  cp ${scriptPath}/mocks/test-12.iyarc .iyarc
   callIya -s high
   testExitCode "they are excluded in .iyarc and min severity is high" "0" "$?"
 
   # test 13
   rm -f package.json
   rm -f yarn.lock
+  rm -rf .yarn
+  rm -rf node_modules
 
-  cp huge-package.json package.json
-  cp huge-yarn.lock yarn.lock
+  cp "${scriptPath}/huge-package.json" package.json
+  cp "${scriptPath}/huge-yarn.lock" yarn.lock
+
+  yarn install
 
   rm -f .iyarc
   touch .iyarc
-  cp mocks/test-13.iyarc .iyarc
+  cp ${scriptPath}/mocks/test-13.iyarc .iyarc
   callIya -s high
   testExitCode "the package JSON has a large number of dependencies" "0" "$?"
 
   rm -f package.json
   rm -f yarn.lock
+  rm -rf .yarn
+  rm -rf node_modules
 
-  cp vunerable-package.json package.json
-  cp vunerable-yarn.lock yarn.lock
+  cp ${scriptPath}/vunerable-package.json package.json
+  cp ${scriptPath}/vunerable-yarn.lock yarn.lock
+
+  yarn install
 
   # test 14
-  excludedAdvisories=$(<mocks/test-14.args)
+  excludedAdvisories=$(<${scriptPath}/mocks/test-14.args)
   callIya -e "${excludedAdvisories},9999,1234"
   testExitCode "some of the exclusions passed via cli are missing" "0" "$?"
 
   # test 15
   rm -f .iyarc
   touch .iyarc
-  cp mocks/test-15.iyarc .iyarc
+  cp ${scriptPath}/mocks/test-15.iyarc .iyarc
   callIya
   testExitCode "some of the exclusions passed via .iyarc are missing" "0" "$?"
 
   # test 16
   rm -f .iyarc
-  excludedAdvisories=$(<mocks/test-16.args)
+  excludedAdvisories=$(<${scriptPath}/mocks/test-16.args)
   callIya -e "${excludedAdvisories}" -f
   testExitCode "some of the exclusions passed via cli are missing and --fail-on-missing-exclusions is passed" "2" "$?"
 
   # test 17
   rm -f .iyarc
   touch .iyarc
-  cp mocks/test-17.iyarc .iyarc
+  cp ${scriptPath}/mocks/test-17.iyarc .iyarc
   callIya -f
   testExitCode "some of the exclusions passed via .iyarc are missing and --fail-on-missing-exclusions is passed" "1" "$?"
 
   # test 18
-  expectedVersion=$(echo $(grep '"version": ' ../package.json | cut -d '"' -f 4))
+  expectedVersion=$(echo $(grep '"version": ' "${scriptPath}/../package.json" | cut -d '"' -f 4))
 
   outputVersion=$(callIya -v 2>&1)
   testExitCode "--version is passed" "1" "$?"
 
   if [ "${outputVersion}" != "${expectedVersion}" ]; then
-    echo "TEST FAILURE: Incorrect version was output: ${outputVersion} - Expected: ${expectedVersion}"
+    echo "✗ TEST FAILURE: Incorrect version was output: ${outputVersion} - Expected: ${expectedVersion}"
     testFailed=true
   fi
 
@@ -168,14 +181,14 @@ runTests() {
 
   # test 20
   callIya -d
-  testExitCode "--debug is passed" "9" "$?"
+  testExitCode "--debug is passed" "12 " "$?"
 
   # test 21
   rm -f .iyarc
   touch .iyarc
-  cp mocks/test-21.iyarc .iyarc
+  cp ${scriptPath}/mocks/test-21.iyarc .iyarc
   callIya
-  testExitCode ".iyarc contains no exclusions" "9" "$?"
+  testExitCode ".iyarc contains no exclusions" "12" "$?"
 }
 
 runTests
